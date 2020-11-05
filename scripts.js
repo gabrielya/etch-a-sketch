6bca64cdb1e6954258de15e6a456f31dc8abc551
Gabe <gabeyastone@gmail.com>
2020-11-04 22:37:23 -0800

    finished

--- scripts.js
+++ scripts.js
@@ -1,30 +1,46 @@
 const container = document.getElementById("container");
 
+//container.style.setProperty("--grid-rows", rows);
+//container.style.setProperty("--grid-cols", cols);
+
 function makeRows(rows, cols) {
-  container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', cols);
-  for (c = 0; c < (rows * cols); c++) {
+  container.style.setProperty("--grid-rows", rows);
+  container.style.setProperty("--grid-cols", cols);
+  for (c = 0; c < rows * cols; c++) {
     let cell = document.createElement("div");
     container.appendChild(cell).className = "grid-item";
-  };
-};
+  }
+}
 
 makeRows(16, 16);
 
 let gridItem = document.querySelectorAll(".grid-item");
 
-gridItem.forEach((cell) => {
-    cell.addEventListener('mouseover', () => {
-        cell.style.background="purple";
+function colorTheGrid() {
+  gridItem.forEach((cell) => {
+    cell.addEventListener("mouseover", () => {
+      cell.style.background = "purple";
     });
-});
+  });
+}
 
-const resetButton = document.getElementById("reset-button");
+colorTheGrid();
 
-const promptv = alert(prompt("Enter how many rows and colums you want.",
-    "rows: ", "cols: "));
+const resetButton = document.querySelector("#reset-button");
 
+resetButton.onclick = () => {
+  do {
+    let promptR = prompt("Enter how many rows and cols you want.");
+  } while (parseInt(promptR) > 100);
 
-resetButton.onclick = () => gridItem.forEach((cell) => {
-        cell.style.background="none";
-});
+  gridItem.forEach((cell) => {
+    cell.remove();
+  });
+
+  let userRows = parseInt(promptR, 10);
+
+  makeRows(userRows, userRows);
+
+  gridItem = document.querySelectorAll(".grid-item");
+  colorTheGrid();
+};
